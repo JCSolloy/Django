e832bd9bb92c858c4b7bbbe973de8d5922a159ad
Solloy <jcsolloy@gmail.com>
2022-05-18 11:45:11 +0100

    django

---
+++ mysite/polls/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Dispositivo(models.Model):
+    Cliente = models.CharField(max_length=50)
+    Proyecto = models.CharField(max_length=50)
+    IP = models.CharField(max_length=20)
+    Registros = models.IntegerField(default=0)
+    Cantidad_de_Registros = models.IntegerField(default=0)
+    Puerto = models.IntegerField(default=0)
+    ID_Labview = models.CharField(max_length=50)
+    Dispositivo_de_Comunicación = models.CharField(max_length=50)
+    Marca_Sensor = models.CharField(max_length=50)
+    Modelo_Sensor = models.CharField(max_length=50)
+    Medición = models.CharField(max_length=50)
---
+++ mysite/polls/views.py
@@ -0,0 +1,54 @@
+from django.http import HttpResponse
+from pyModbusTCP.client import ModbusClient
+from django.template import loader
+from .models import Dispositivo
+
+def index(request):
+
+    c = ModbusClient(host="10.232.3.41",
+                 port=502,
+                 unit_id=0,
+                 debug=False,
+                 auto_open=True,
+                 auto_close=True)
+
+    regs = c.read_holding_registers(4419, 10)
+
+
+    registros = 'Registros interrogados ' + str(regs)
+
+    return HttpResponse(registros)
+
+def home(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def modbus(request):
+    Dispositivos = Dispositivo.objects.all().values()
+    template = loader.get_template('modbus.html')
+    if request.method == 'POST':
+        Cliente = request.POST['Cliente']
+        Proyecto = Dispositivo.objects.filter(Cliente=Cliente).values()
+        c = ModbusClient(
+                 host="10.97.0.188",
+                 port=502,
+                 unit_id=0,
+                 debug=False,
+                 auto_open=True,
+                 auto_close=True)
+
+        regs = c.read_holding_registers(4419, 10)
+
+        registros = 'Registros interrogados ' + str(regs) 
+
+        context = {
+            'Dispositivos' : Dispositivos,
+            'Registros' : registros,
+            }
+    else:
+
+        context = {
+            'Dispositivos' : Dispositivos,
+            'Registros' : ' ',
+            }
+    return HttpResponse(template.render(context, request))
